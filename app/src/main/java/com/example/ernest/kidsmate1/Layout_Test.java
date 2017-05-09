6f3dc43815073762437a6249fc50b4e443a069e8
collarhub <june4145@naver.com>
2017-05-09 18:55:16 +0900

    커스텀 레이아웃 테스트 완료 imageguessing에 시험적용

--- app/src/main/java/com/example/ernest/kidsmate1/Layout_Test.java
+++ app/src/main/java/com/example/ernest/kidsmate1/Layout_Test.java
@@ -1,6 +1,9 @@
 package com.example.ernest.kidsmate1;
 
 import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
@@ -17,6 +20,14 @@ public class Layout_Test extends AppCompatActivity {
     private Test_GameView gameView;
     private MyImageView myCharacter;
     private MyButton mic;
+    private MyButton pass;
+    private MyButton play;
+    private MyButton submit;
+    private MyButton input;
+    private MyTextView quiz;
+    private MyTextView hint;
+    private MyImageView enemy;
+    private MyImageView imageQuiz;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,11 +42,61 @@ public class Layout_Test extends AppCompatActivity {
 
         mic = gameView.getMic();
         mic.setImage(R.drawable.buttonmic);
+        mic.setText("시작");
         mic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "시작", Toast.LENGTH_SHORT).show();
+                mic.setText("연결됨");
+                mic.setEnabled(false);
             }
         });
+
+        pass = gameView.getPass();
+        pass.setImage(R.drawable.pass);
+        pass.setText("패스!");
+        pass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "패스!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        play = gameView.getPlay();
+        play.setImage(R.drawable.play);
+        play.setText("재생");
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "재생", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        submit = gameView.getSubmit();
+        submit.setImage(R.drawable.submit);
+        submit.setText("제출");
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "제출", Toast.LENGTH_SHORT).show();
+                input.setText("");
+            }
+        });
+
+        input = gameView.getInput();
+        input.setImage(R.drawable.input);
+
+        //quiz = gameView.getQuizBlankGuessing();
+        //quiz.setText("apple");
+
+        hint = gameView.getHint();
+        hint.setText("aaaaaaaaaaaaaaaaaaaaaaaa\nbbbbbbbbbbbbbbbbbbbbbbbb\ncccccccccccccccccc\n\ndddddddddddddddddddddddddd");
+
+        enemy = gameView.getEnemy();
+        enemy.setImage(R.drawable.bosssphinx);
+
+        imageQuiz = gameView.getQuizImageGuessing();
+        Drawable drawable = getResources().getDrawable(R.drawable.testwordchain);
+        imageQuiz.setBackground(drawable);
     }
 }
